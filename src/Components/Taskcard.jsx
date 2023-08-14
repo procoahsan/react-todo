1846067c7028c452a7605c9303fa73526e3b9e4c
procoahsan <82664418+procoahsan@users.noreply.github.com>
2023-08-14 15:57:51 +0500

    update func

--- src/Components/Taskcard.jsx
+++ src/Components/Taskcard.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-export const Taskcard = ({title,completed}) => {
+export const Taskcard = ({id,title,completed , update}) => {
   return (
     <div
-      className="bg-white  d-flex flex-row justify-content-between align-items-center px-2 rounded"
+      className="bg-white container  d-flex flex-row justify-content-between align-items-center px-2 rounded"
       style={{ height: "50px", width: "450px" }}
     >
       <div class="form-check">
         <input
           class="form-check-input"
           type="checkbox"
-          value={completed}
-          id="flexCheckChecked"
+          checked={completed} // Use 'checked' instead of 'value' for checkbox state
+          id={`flexCheckChecked-${id}`}
+          onChange={()=>update(id)}
         />
         <label class="form-check-label" for="flexCheckChecked">
           {title}
--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -4,27 +4,72 @@ export const Todo = () => {
   const [taskArray, setArray] = useState([])
   const [title , setTitle] = useState("")
 
-  const addTodo = ()=>{
-    let result = {}
-    result["title"] = title
-    result["completed"] = false
-    let array = [...taskArray , result]
-    setArray(array)
-
+  const addTodo = () => {
+    let result = {};
+    if (taskArray.length === 0) {
+      result['id'] = 1;
+    } else {
+      const lastId = taskArray[taskArray.length - 1].id;
+      result['id'] = lastId + 1;
+    }
+    result["title"] = title; // Make sure 'title' is defined somewhere
+    result["completed"] = false;
+  
+    let array = [...taskArray, result];
+    setArray(array); // Assuming setArray is a function to update the taskArray
+    // console.log(taskArray)
+  };
+  const viewArray = ()=>{
+    console.log(taskArray)
   }
+  const updateFunction = (id) =>{
+    const updatedArray = taskArray.map((data) => {
+      if (data.id === id) {
+        return { ...data, completed: true };
+      }
+      return data;
+    });
+    setArray(updatedArray);
+  }
+  
   return (
     <div className="container-fluid bg-dark d-flex flex-column align-items-center justify-content-center vh-100 w-100 gap-4">
-      <span className="text-white display-5">Todo list</span>
-      <input placeholder="Enter the task" type="text" className="w-25 h5 px-3 rounded" style={{height:"50px"}} value={title} onChange={(e)=>setTitle(e.target.value)}/>
-      <button className="rounded btn btn-success " style={{width:"150px" , height : "50px"}} onClick={addTodo}>Add todo</button>
-      {
-        taskArray.map((data)=>{
-          return(
-            <Taskcard title={data.title} completed = {data.completed} />
-          )
-        })
-      }
-      
+    <div className="row">
+      <div className="col-md-8 col-sm-10 col-xs-12 mx-auto">
+        <span className="text-white display-5">Todo list</span>
+        <input
+          placeholder="Enter the task"
+          type="text"
+          className="w-100 h5 px-3 rounded"
+          style={{ height: "50px" }}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <button
+          className="rounded btn btn-success my-2"
+          style={{ width: "100%", height: "50px" }}
+          onClick={addTodo}
+        >
+          Add todo
+        </button>
+        <button
+          className="rounded btn btn-success my-2"
+          style={{ width: "100%", height: "50px" }}
+          onClick={viewArray}
+        >
+          View Array
+        </button>
+        {taskArray.map((data) => (
+          <Taskcard
+            key={data.id}
+            id={data.id}
+            title={data.title}
+            completed={data.completed}
+            update={updateFunction}
+          />
+        ))}
+      </div>
     </div>
+  </div>
   );
 };
